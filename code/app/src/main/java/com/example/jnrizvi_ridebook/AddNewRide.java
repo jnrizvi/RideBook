bc191175cba3a1f75df0f8f6f853ca2e5d150f8c
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-09-21 16:09:10 -0600

    made activity for adding a new ride

---
+++ code/app/src/main/java/com/example/jnrizvi_ridebook/AddNewRide.java
@@ -0,0 +1,14 @@
+package com.example.jnrizvi_ridebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewRide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_ride);
+    }
+}
--- code/app/src/main/java/com/example/jnrizvi_ridebook/MainActivity.java
+++ code/app/src/main/java/com/example/jnrizvi_ridebook/MainActivity.java
@@ -15,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Button addNew_button = (Button) findViewById(R.id.add_new);
+        Button seeTotal_button = (Button) findViewById(R.id.see_total);
 
-        Button button = (Button) findViewById(R.id.see_total);
+        addNew_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddNewRide.class);
+                startActivity(intent);
+            }
+        });
 
-        button.setOnClickListener(new View.OnClickListener() {
+        seeTotal_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ViewTotalDistance.class);
