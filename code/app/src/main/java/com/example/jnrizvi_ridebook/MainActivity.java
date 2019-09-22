56903748ced85f819c11cf369b55b104ae320f70
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-09-21 22:16:56 -0600

    setOnItemClick works for ListView items

--- code/app/src/main/java/com/example/jnrizvi_ridebook/MainActivity.java
+++ code/app/src/main/java/com/example/jnrizvi_ridebook/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     ListView ridesListView;
     TextView textView;
     String[] listOfStrings;
+    int index = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +29,17 @@ public class MainActivity extends AppCompatActivity {
         final ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, R.layout.list, listOfStrings);
         ridesListView.setAdapter(myAdapter);
 
-//        ridesListView.setOnClickListener(new AdapterView().getOnItemClickListener() {});
+        ridesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String val = ridesListView.getItemAtPosition(i).toString();
+//                index = i;
+//                ridesListView.setSelection(i);
+                Intent intent = new Intent(getApplicationContext(), AddNewRide.class);
+                startActivity(intent);
+            }
+
+        });
 
         Button addNew_button = (Button) findViewById(R.id.add_new);
         Button seeTotal_button = (Button) findViewById(R.id.see_total);
